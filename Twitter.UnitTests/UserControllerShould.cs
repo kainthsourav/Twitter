fe98ea51e5882ff4847154aca3d93a28cb294a6c
kainthsourav <souravkainth@outlook.com>
2022-04-22 23:32:30 +0530

    UT Added

--- Twitter.UnitTests/UserControllerShould.cs
+++ Twitter.UnitTests/UserControllerShould.cs
@@ -1,20 +1,34 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Twitter.Controllers;
+using Twitter.DataAccess;
+using Twitter.ViewModel;
+using Xunit;
+using Twitter.Models;
+
 namespace Twitter.UnitTests
 {
-    [TestFixture]
     public class UserControllerShould
     {
-        /*private readonly Mock<IEmployeeRepository> _mockRepo;
-        private readonly EmployeesController _controller;
-        public EmployeesControllerTests()
+        private readonly DataContext _mockContext;
+        private readonly UserController _controller;
+        public UserControllerShould()
         {
-            _mockRepo = new Mock<IEmployeeRepository>();
-            _controller = new EmployeesController(_mockRepo.Object);
-        }*/
-        [Test]
-        public void Index()
+            _controller = new UserController(_mockContext);
+        }
+        [Fact]
+        public void Should_ReturnViewForSignIn()
         {
-
+            var result = _controller.SignIn();
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void Should_ReturnViewForRegister()
+        {
+            var result = _controller.Register();
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
