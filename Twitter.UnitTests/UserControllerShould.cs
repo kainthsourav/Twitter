dbda62034343f6c73d01097a7666623d6f518abd
kainthsourav <souravkainth@outlook.com>
2022-04-22 22:42:26 +0530

    Updated Table Query

---
+++ Twitter.UnitTests/UserControllerShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+namespace Twitter.UnitTests
+{
+    [TestFixture]
+    public class UserControllerShould
+    {
+        /*private readonly Mock<IEmployeeRepository> _mockRepo;
+        private readonly EmployeesController _controller;
+        public EmployeesControllerTests()
+        {
+            _mockRepo = new Mock<IEmployeeRepository>();
+            _controller = new EmployeesController(_mockRepo.Object);
+        }*/
+        [Test]
+        public void Index()
+        {
+
+        }
+    }
+}
